e118475792a011d38926c0f865389fe326669e23
CHEN ZHAOHUIUI <cccisi@live.com>
2018-03-12 14:06:00 +0800

    Initial commit

---
+++ src/createNetwork.java
@@ -0,0 +1,10 @@
+import org.neuroph.util.TransferFunctionType;
+
+public class createNetwork {
+
+    private void creatNetwork(int inputNeuronsCount, int outputNeuronsCount, TransferFunctionType){
+
+        this.setNetworkType()
+    }
+
+}
